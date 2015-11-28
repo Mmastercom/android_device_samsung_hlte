febf5cebde755fcfe00ffdba0ef950cff40a5830
lordeko <mattia.ruggeri88@gmail.com>
2015-11-28 12:14:48 +0100

    Update orion.mk

--- orion.mk
+++ orion.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/orion/config/common_full_phone.mk)
 
 # Inherit device specific
-$(call inherit-product, device/motorola/falcon/full_hlte.mk)
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 PRODUCT_RELEASE_NAME := NOTE 3
 PRODUCT_DEVICE := hlte
